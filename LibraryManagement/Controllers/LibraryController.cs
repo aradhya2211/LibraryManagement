df72cf4b96ad25d2bd184ba5f98b4874ad830c08
Aradhya Saxena <aradhya2211@gmail.com>
2022-01-18 17:17:52 +0530

    Created API for Books Collection...

--- LibraryManagement/Controllers/LibraryController.cs
+++ LibraryManagement/Controllers/LibraryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LibraryManagement.Models;
+using LibraryManagement.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,11 +13,77 @@ namespace LibraryManagement.Controllers
     [ApiController]
     public class LibraryController : ControllerBase
     {
+        public BookServices BookService = new BookServices();
         [HttpGet]
-        public String[] GetBooks()
+        public async Task<ActionResult> GetBooksAsync()
         {
-            string[] Books = { "Book1", "Book2", "Book3" };
-            return Books;
+            try
+            {
+                var results = await BookService.GetAllBooks();
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return NotFound(e.Message);
+            }
+        }
+        [HttpGet("/{Name}")]
+        public async Task<ActionResult> GetBookByName(String Name)
+        {
+            try
+            {
+                var results = await BookService.GetBookByName(Name);
+                return Ok(results);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return NotFound(e.Message);
+            }
+        }
+        [HttpPost]
+        public async Task<ActionResult> InsertBook(Book NewBook)
+        {
+            try
+            {
+                var result = await BookService.InsertNewBook(NewBook);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+        [HttpDelete]
+        public async Task<ActionResult> DeleteBook(Book BookToBeDeleted)
+        {
+            try
+            {
+                var DeletedBook = await BookService.DeleteBook(BookToBeDeleted);
+                return Ok(DeletedBook);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateBook(Book BookToBeUpdated)
+        {
+            try
+            {
+                var UpdatedBook = await BookService.UpdateBook(BookToBeUpdated);
+                return Ok(UpdatedBook);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+                throw;
+            }
         }
     }
 }
---
+++ LibraryManagement/Services/IBookServices.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Services
+{
+    interface IBookServices
+    {
+        public Task<List<Book>> GetAllBooks();
+        public Task<Book> InsertNewBook(Book NewBook);
+        public Task<Book> DeleteBook(Book BookToBeDeleted);
+        public Task<List<Book>> GetBookByName(String Name);
+        public Task<Book> UpdateBook(Book BookToBeUpdated);
+        public Task<Book> GetBookById(Book BookById);
+    }
+}
